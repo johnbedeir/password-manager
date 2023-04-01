ec5be2b688cd69bc70291512f9122f524ca87f82
johnbedeir <john.bedeir@gmail.com>
2023-04-01 22:37:28 +0200

    split functions

--- main.go
+++ main.go
@@ -13,17 +13,21 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func main() {
-	// Get user input for password name
+func getPassName() string {
 	fmt.Print("Enter name for the password: ")
 	var pwName string
 	fmt.Scanln(&pwName)
+	return pwName
+}
 
+func getUserName() string {
 	fmt.Print("Enter user name: ")
 	var uName string
 	fmt.Scanln(&uName)
+	return uName
+}
 
-	// Generate password
+func generatePass() string {
 	rand.Seed(time.Now().UnixNano())
 	pwLength := 16
 	pwChars := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()-_=+[{]}\\|;:'\",<.>/?"
@@ -31,12 +35,14 @@ func main() {
 	for i := 0; i < pwLength; i++ {
 		pw.WriteByte(pwChars[rand.Intn(len(pwChars))])
 	}
+	return pw.String()
+}
 
-	// Connect to MongoDB and store password
+func databaseConnect(pwName, uName, password string) (*mongo.Collection, error) {
 	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
 	client, err := mongo.Connect(context.Background(), clientOptions)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 	defer client.Disconnect(context.Background())
 
@@ -48,11 +54,25 @@ func main() {
 		Password string `bson:"password"`
 	}
 
-	_, err = collection.InsertOne(context.Background(), bson.M{"name": pwName, "username": uName, "password": pw.String()})
+	_, err = collection.InsertOne(context.Background(), bson.M{"name": pwName, "username": uName, "password": password})
+	if err != nil {
+		return nil, err
+	}
+	return collection, nil
+}
+
+func printPass(pwName, password string, collectionName string) {
+	fmt.Println("Generated password:", password)
+	fmt.Println("Password stored in MongoDB database with name", pwName, "in collection", collectionName)
+}
+
+func main() {
+	pwName := getPassName()
+	uName := getUserName()
+	password := generatePass()
+	collection, err := databaseConnect(pwName, uName, password)
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	fmt.Println("Generated password:", pw.String())
-	fmt.Println("Password stored in MongoDB database with name", pwName, "in collection", collectionName)
+	printPass(pwName, password, collection.Name())
 }
